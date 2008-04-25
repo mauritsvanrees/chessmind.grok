009262112e0995001c14d000768bf4f29b77b89f
Maurits van Rees <maurits@vanrees.org>
2008-04-25 23:09:45 +0000

    rename chessmindgrok to chessmind.grok
    svn path=/chessmind.grok/trunk/; revision=1286

--- src/chessmindgrok/testing.py
+++ src/chessmindgrok/testing.py
@@ -1,7 +1,7 @@
 import os.path
-import chessmindgrok
+import chessmind.grok
 from zope.app.testing.functional import ZCMLLayer
 
 ftesting_zcml = os.path.join(
-    os.path.dirname(chessmindgrok.__file__), 'ftesting.zcml')
+    os.path.dirname(chessmind.grok.__file__), 'ftesting.zcml')
 FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__, 'FunctionalLayer')
