fb4cb21feb706f6272583fa7798a6f4c7b23a49a
Maurits van Rees <maurits@vanrees.org>
2008-04-26 00:36:32 +0000

    fill in setup.py and add svn ignore for dist and build
    svn path=/chessmind.grok/trunk/; revision=1301

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ version = '0.3'
 
 setup(name='chessmind.grok',
       version=version,
-      description="",
-      long_description="""\
+      description="Grok UI for ChessMind",
+      long_description="""Me Grok Play Chess!\
 """,
       # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[], 
-      keywords="",
-      author="",
-      author_email="",
-      url="",
-      license="",
+      keywords="chess, grok",
+      author="Maurits van Rees",
+      author_email="maurits@vanrees.org",
+      url="http://maurits.vanrees.org/",
+      license="GPL",
       package_dir={'': 'src'},
       packages=find_packages('src'),
       include_package_data=True,
